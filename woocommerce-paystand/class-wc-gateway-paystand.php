f73ec4b62c0ff497550dbf550d856d611aaf4723
Omar Baqueiro <obaqueiro@paystand.com>
2018-06-19 13:01:16 -0500

    Move httpful library to includes

--- woocommerce-paystand/class-wc-gateway-paystand.php
+++ woocommerce-paystand/class-wc-gateway-paystand.php
@@ -3,7 +3,7 @@
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
-include('httpful.phar');
+include( plugin_dir_path( __FILE__ ) . 'includes/httpful.phar');
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
